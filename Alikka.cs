e4ecd8e7eb44381f7059f475e12df6b3eaab6442
Alikka <42414390+alikkakold@users.noreply.github.com>
2022-02-18 14:19:05 +0300

    Update Alikka.cs

--- Alikka.cs
+++ Alikka.cs
@@ -54,7 +54,7 @@ namespace ETSBots.ETSBots.CandleFormations
 					if (LongPos.Count == 0 && ShortPos.Count == 0)
                     {
 						if (Reopening)
-						BuyGreater(bar + 1, priceDown[bar + 1] - Otstup_Open.ValueInt * FinInfo.Security.MinStep, Volume_First_Position.Value,
+						BuyAtLimit (bar + 1, priceDown[bar + 1] - Otstup_Open.ValueInt * FinInfo.Security.MinStep, Volume_First_Position.Value,
                             "Open 1");
                         BuyGreater(bar + 1, priceDown[bar + 1] - Otstup_Open.ValueInt * FinInfo.Security.MinStep, Volume_Second_Position.Value,
                             "Open 2");
